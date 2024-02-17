f2862576ba908228d3c81e75d6128b8481ec2cc9
Mario Romero Aguilar <mromagu2611@g.educaand.es>
2024-02-17 18:39:13 +0100

    Scraper con peticion get

--- src/main/java/WebScraper/Scraper.java
+++ src/main/java/WebScraper/Scraper.java
@@ -5,6 +5,10 @@
 package WebScraper;
 
 import java.io.BufferedReader;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
@@ -12,8 +16,8 @@ import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -29,42 +33,52 @@ public class Scraper {
     /**
      * @param args the command line arguments
      */
+    private static String nombre;
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String direccionWeb = "http://localhost:5000";
+        String direccionWeb = "http://localhost:5000/GET";
         ascii();
         System.out.println("Escribe un nombre.");
-        String nombre = sc.nextLine();
-        urlGET(direccionWeb, nombre);
+        nombre = sc.nextLine();
+        urlGET(direccionWeb);
     }
 
-    private static void urlGET(String direccionWeb, String nombre) {
+    private static void urlGET(String direccionWeb) {
         try {
             URI uri = new URI(direccionWeb);
             URL url = uri.toURL();
-            procesarSolicitud(url, nombre);
+            System.out.println(url);
+            procesarSolicitud(url);
         } catch (URISyntaxException | MalformedURLException ex) {
             Logger.getLogger(Scraper.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
 
-    private static void procesarSolicitud(URL url, String nombre) {
+    private static void procesarSolicitud(URL url) {
         try {
             HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
-            Map<String, List<String>> headers = leerHeader(urlConnection);
-            String contentType = urlConnection.getContentType();
-            int contentLength = urlConnection.getContentLength();
             StringBuffer body = leerBody(urlConnection);
-//            imprimirResultadoLeido(headers, contentType, contentLength, body.toString());
-            extraerDatosTabla(body.toString(), nombre);
+            System.out.println(body.toString());
+            String action = extraerFormAction(body.toString());
+            System.out.println(action);
+            String selectName = extraerSelectName(body.toString());
+            System.out.println(selectName);
+            ArrayList<String> opciones = extraerOpciones(body.toString());
+            System.out.println(opciones.toString());
+            List<String> partidasJugadas = new ArrayList<>();
+            List<String> partidasGanadas = new ArrayList<>();
+            for (String op : opciones) {
+                String respuesta = conexionGET(action + "?" + selectName + "=" + op);
+                partidasJugadas.add(obtenerPartidasJugadas(respuesta, nombre));
+                partidasGanadas.add(obtenerPartidasGanadas(respuesta, nombre));
+            }
+            System.out.println("Partidas jugadas por " + nombre + ": " + partidasJugadas.toString());
+            System.out.println("Partidas ganadas por " + nombre + ": " + partidasGanadas.toString());
+
         } catch (IOException ex) {
             Logger.getLogger(Scraper.class.getName()).log(Level.SEVERE, null, ex);
         }
-    }
-
-    private static Map<String, List<String>> leerHeader(HttpURLConnection urlConnection) {
-        Map<String, List<String>> headersResponse = urlConnection.getHeaderFields();
-        return headersResponse;
     }
 
     private static StringBuffer leerBody(HttpURLConnection urlConnection) {
@@ -89,66 +103,137 @@ public class Scraper {
         return null;
     }
 
-    private static void extraerDatosTabla(String body, String nombre) {
-        Pattern pattern = Pattern.compile("<tr>(.*?)</tr>", Pattern.DOTALL);
+    // Metodo para extraer la accion del formulario
+    private static String extraerFormAction(String body) {
+        // Expresión regular para encontrar un valor.
+        String regex = "<form[^>]*action=\"([^\"]+)\"[^>]*>";// También conocidas como regex, son patrones de búsqueda que se utilizan para encontrar coincidencias dentro de cadenas de texto.
+
+        // Compilar la expresión regular en un patrón
+        Pattern pattern = Pattern.compile(regex);//Se utiliza para representar una expresión regular compilada.
+
+        // Crear un matcher para el cuerpo del HTML
+        Matcher matcher = pattern.matcher(body);//Es una clase que se encarga de realizar la búsqueda de la expresión regular en una cadena de texto específica.
+
+        // Si se encuentra una coincidencia, extraer el valor del atributo "action"
+        if (matcher.find()) {//Este método busca la siguiente coincidencia de la expresión regular en la cadena de texto analizada
+            return matcher.group(1);//Se utiliza para obtener el texto que coincide con el primer grupo capturado por la expresión regular
+        } else {
+            return "";
+        }
+        /**
+         * *
+         * Los grupos capturados por una expresión regular son partes
+         * específicas de la cadena que coinciden con ciertas partes del patrón
+         * definido en la expresión regular. Los paréntesis (( y )) se utilizan
+         * para definir grupos en una expresión regular.
+         */
+    }
+
+    private static String extraerSelectName(String body) {
+        String regex = "<select[^>]*name=\"([^\"]+)\"[^>]*>";
+        Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(body);
+        if (matcher.find()) {
+            return matcher.group(1);
+        } else {
+            return "";
+        }
+    }
 
-        int ganadas = 0;
-        int perdidas = 0;
+    //Necesita implemtnear la dependecias de org.jsoup
+    private static ArrayList<String> extraerOpciones(String body) {
+        ArrayList<String> opciones = new ArrayList<>();
+        Document doc = Jsoup.parse(body);
 
-        while (matcher.find()) {
-            String fila = matcher.group(1); // Obtener el contenido de la fila entre <tr> y </tr>
-//            System.out.println(fila);
-            fila = fila.replaceAll("<[^>]*>", " ");
-//            System.out.println(fila);
-            if (fila.contains(nombre)) {
-                // Dividir el string en palabras
-                String[] words = fila.split("\\s+");
-                // Obtener la última palabra
-                String lastWord = words[words.length - 1];
-//                System.out.println("--" + fila);
-//                System.out.println("+"+lastWord);
-                if (lastWord.equals(nombre)) {
-                    ganadas++;
-                } else {
-                    perdidas++;
+        // Encuentra todos los elementos select
+        Elements selects = doc.select("select");
+
+        // Itera sobre los elementos select
+        for (Element select : selects) {
+            // Encuentra todas las opciones dentro del select actual
+            Elements options = select.select("option");
+
+            // Itera sobre las opciones y extrae sus valores
+            for (Element option : options) {
+                String optionValue = option.attr("value");
+                opciones.add(optionValue);
+            }
+        }
+
+        return opciones;
+    }
+
+    private static String conexionGET(String peticionGET) {
+        String body = "";
+        try {
+            URI uri = new URI(peticionGET);
+            URL url = uri.toURL();
+            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+
+            // Lee el cuerpo de la respuesta
+            String linea;
+            StringBuilder sb = new StringBuilder();
+            BufferedReader lector = new BufferedReader(new InputStreamReader(urlConnection.getInputStream(), "UTF-8"));
+            while ((linea = lector.readLine()) != null) {
+                sb.append(linea);
+            }
+            body = sb.toString();
+
+        } catch (IOException | URISyntaxException e) {
+            System.out.println("Error: conexionGet");
+        }
+
+        return body;
+    }
+
+    private static String obtenerPartidasJugadas(String html, String jugador) {
+        String partidasJugadas = null;
+        Document doc = Jsoup.parse(html);
+        Elements tablas = doc.select(".contenedor-tableros");
+
+        for (Element tabla : tablas) {
+            Elements h2s = tabla.select("h2");
+            for (Element h2 : h2s) {
+                if (h2.text().contains(jugador)) {
+                    partidasJugadas = h2.text();
                 }
             }
         }
 
-        System.out.println("Cantidad de partidas Ganadas: " + ganadas);
-        System.out.println("Cantidad de partidas Perdidas: " + perdidas);
-
+        return partidasJugadas;
     }
 
-    private static void imprimirResultadoLeido(Map<String, List<String>> headers, String contentType, int contentLength, String body) {
-        System.out.println("Todos los headers leidos");
-        System.out.println(headers);
+    private static String obtenerPartidasGanadas(String html, String jugador) {
+        String partidasGanadas = null;
+        Document doc = Jsoup.parse(html);
+        Elements tablas = doc.select(".contenedor-tableros");
 
-        // Imprimir el tipo de contenido y la longitud del contenido de la respuesta HTTP
-        System.out.println("Header Content-Type");
-        System.out.println(contentType);
-        System.out.println("Header Content-Length");
-        System.out.println(contentLength);
+        for (Element tabla : tablas) {
+            Elements h2s = tabla.select("h2");
+            for (Element h2 : h2s) {
+                if (h2.text().contains(jugador)) {
+                    Element tablaPadre = tabla.parent();
+                    Element h1 = tablaPadre.selectFirst("h1");
+                    if (h1 != null && h1.text().contains(jugador)) {
+                        partidasGanadas = h1.text();
+                    }
+                }
+            }
+        }
 
-        // Imprimir el cuerpo de la respuesta HTTP
-        System.out.println("Body Leido:");
-        System.out.println(body);
-
-        // Eliminar todas las etiquetas HTML del cuerpo de la respuesta
-        body = body.replaceAll("<[^>]*>", "");
-        System.out.println("Body Leido eliminando todas las etiquetas, nos queda solo el 'contents':");
+        return partidasGanadas;
     }
 
     private static void ascii() {
-        System.out.println("    ________\n"
-                + "   /_______/\\\n"
-                + "   \\ \\    / /\n"
-                + " ___\\ \\__/_/___\n"
-                + "/____\\ \\______/\\\n"
-                + "\\ \\   \\/ /   / /\n"
-                + " \\ \\  / /\\  / /\n"
-                + "  \\ \\/ /\\ \\/ /\n"
-                + "   \\_\\/  \\_\\/");
+        System.out.println("""
+                               ________
+                              /_______/\\
+                              \\ \\    / /
+                            ___\\ \\__/_/___
+                           /____\\ \\______/\\
+                           \\ \\   \\/ /   / /
+                            \\ \\  / /\\  / /
+                             \\ \\/ /\\ \\/ /
+                              \\_\\/  \\_\\/""");
     }
 }
